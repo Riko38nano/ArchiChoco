fd4ddbf7df7f6078c79fb68f0b57d52878774c6d
Riko38nano <55441400+Riko38nano@users.noreply.github.com>
2021-05-11 10:02:57 +0200

    Update connector.js

--- bdd/connector.js
+++ bdd/connector.js
@@ -1,11 +1,14 @@
+import {HOST_NAME, USER, PASSWORD, DATABASE} frome "../utils/constants/.env"
+
 const mysql = require('mysql');
+
 // fonction de connexion à la base de donnée
 const connect = function () {
     const connected = mysql.createConnection({
-        host: "onnjomlc4vqc55fw.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
-        user: 'gpmbr8v59adpq3j5',
-        password: 'xf5dts2fx5o1bcgz',
-        database: "ye2ksnnef7sraepi"
+        host: HOST_NAME,
+        user: USER,
+        password: PASSWORD,
+        database: DATABASE
     });
 
     connected.connect();
